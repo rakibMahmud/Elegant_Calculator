614a2682b0f9ea16d47b669a2f05f68c10a8d09b
rakib-hasan <mahmud1807077@stud.kuet.ac.bd>
2022-05-16 22:33:51 +0600

    Demo Created

---
+++ app/src/main/java/com/example/new_sample/Demo.java
@@ -0,0 +1,16 @@
+class Demo
+{
+    int a;
+
+    public Demo(int a) {
+        this.a = a;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
